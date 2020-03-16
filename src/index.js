99b34622a67bcca2170f7f476c9daa67f84c6d93
monsieurverde <domverdemgmt@gmail.com>
2020-03-16 19:23:11 -0400

    index js landing page

--- src/index.js
+++ src/index.js
@@ -16,10 +16,16 @@ var hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
+      {/*
       <Route path="/landing-page" component={LandingPage} />
       <Route path="/profile-page" component={ProfilePage} />
       <Route path="/login-page" component={LoginPage} />
       <Route path="/" component={Components} />
+      */}
+      <Route path="/" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/landing-page" component={Components} />
     </Switch>
   </Router>,
   document.getElementById("root")
